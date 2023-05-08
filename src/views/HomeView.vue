0399f4c368ae241f548e9bc57e47fd99ca654cca
mjwfilippo <mjwfilippo@gmail.com>
2023-05-08 08:58:39 +0200

    successfully configured the settings for linters

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -12,7 +15,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 };
 </script>
